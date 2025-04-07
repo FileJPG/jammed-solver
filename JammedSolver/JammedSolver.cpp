a4dca0fcd5b790a62d84c7b6f66bb04aad76deac
FileJPG <valen.sir@mail.ru>
2025-04-07 21:27:53 +0300

    Create main.cpp
    Create main.cpp for main function of program.

--- JammedSolver/JammedSolver.cpp
+++ JammedSolver/JammedSolver.cpp
@@ -1,11 +1,2 @@
-﻿#include <iostream>
-#include "JammedSolver.h"
-#include "GameFieldData.h"
-#include "PriorityQueue.h"
-#include "Node.h"
-#include "Error.h"
+﻿#include "JammedSolver.h"
 
-int main(int argc, char* argv[])
-{
-    return 0;
-}
---
+++ JammedSolver/main.cpp
@@ -0,0 +1,40 @@
+/*!
+* \file
+* \brief Данный файл содержит главную функцию программы JammedSolver.
+*
+* \mainpage Документация для программы "JammedSolver"
+Программа предназначена для осуществления решения игры "Пятнашки".
+Для функционирования программы необходима операционная система Windows 7 или выше.
+Программа разработана на языке С++ с использованием стандартных библиотек C++.
+Программа должна получать два аргумента командной строки: имя входного файла и имя выходного файла в формате 'txt'.
+
+Пример команды запуска программы:
+* \code
+./JammedSolver.exe ./input.txt ./output.txt
+* \endcode
+
+* \author Ivan Maltsev
+* \date June 2025
+* \version 1.0
+*/
+
+#include "Structure.h"
+#include "JammedSolver.h"
+#include "GameFieldData.h"
+#include "PriorityQueue.h"
+#include "Node.h"
+#include "Error.h"
+
+/*!
+ * \brief Главная функция программы
+ * \param [in] argc - количество переданных аргументов командной строки
+ * \param [in] argv - аргументы командной строки
+ * \param [in] argv[0] - аргумент запуска программы
+ * \param [in] argv[1] - путь к входному файлу
+ * \param [in] argv[2] - путь к выходному файлу
+ * \return 0 - программа завершилась без исключений
+ */
+int main(int argc, char* argv[])
+{
+    return 0;
+}
