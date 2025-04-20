3e88387432fd574a8c5dffbcaef9d2c6cd312370
FileJPG <valen.sir@mail.ru>
2025-04-20 20:30:49 +0300

    Release Node and PriorityQueue classes
    Release is .h becouse of template classes, delete of .cpp of Node and PriorityQueue

--- JammedSolver/Node.h
+++ JammedSolver/Node.h
@@ -23,9 +23,36 @@ template<class T> class Node
 public:
 
 	/*!
+	* \brief Конструктор с параметрами
+	*/
+	Node(T data, Node<T>* parent = nullptr, vector<Node<T>*> children = {}) : data(data), parent(parent), children(children) {}
+
+	/*!
+	* \brief Геттер для поля data
+	*/
+	T getData() const { return data; }
+
+	/*!
 	* \brief Получить путь от корня до узла
 	* \param [in] currentNode - текущий узел, до которого нужно получить путь
 	* \return Узлы последовательно от корня до текущего. Пустой вектор, если граф не является деревом
 	*/
-	vector<Node<T>*> getWayFromRoot(const Node<T>* currentNode) const;
+	vector<Node<T>*> getWayFromRoot(Node<T>* currentNode) const
+	{
+		// Выйти из функции, если входные данные не валидные
+		if (currentNode == nullptr) return {};
+
+		vector<Node<T>*> way; // ...Создать результирующий вектор для записи в него узлов из рекурсивной функции
+		
+		// #Выполнить данную функцию для родителя текущего узла#, если текущий узел не является корнем и родитель текущего узла не является начальным узлом
+		if (currentNode->parent != nullptr && currentNode->parent != this)
+			way = getWayFromRoot(currentNode->parent);
+
+		// Положить текущий узел в результирующий вектор, если текущий узел является корнем или результирующий вектор не пустой
+		if (currentNode->parent == nullptr || !way.empty())
+			way.push_back(currentNode);
+
+		// Вернуть результирующий вектор
+		return way;
+	}
 };
--- JammedSolver/PriorityQueue.h
+++ JammedSolver/PriorityQueue.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
@@ -20,16 +21,53 @@ template<class T> class PriorityQueue
 
 public:
 
+	/*!
+	* \brief Геттер для поля items
+	*/
+	const vector<T*>& getItems() const { return items; }
+
+	/*!
+	* \brief Проверить, является ли очередь пустой
+	* \return Очередь является пустой
+	*/
+	bool isEmpty() const { return items.empty(); }
+
     /*!
     * \brief Добавить в очередь элемент с приоритетом
     * \param [in] item - новый элемент
     * \param [in] priority - приоритет элемента
     */
-    void enqueue(const T* item, const int& priority);
+    void enqueue(T* item, int priority)
+	{
+		// Добавить в конец вектора элементов очереди новый элемент
+		items.push_back(item);
+
+		// Добавить в конец вектора приоритетов элементов очереди приоритет нового элемента
+		priorities.push_back(priority);
+	}
 
     /*!
     * \brief Взять из очереди элемент с наибольшим приоритетом (наименьшим значением)
     * \return Элемент очереди с наибольшим приоритетом. В случае множества таких элементов выбирается тот, который был добавлен раньше
     */
-    T* dequeue();
+    T* dequeue()
+	{
+		// Вернуть нулевой указатель, если в очереди нет элементов
+		if (items.empty())
+			return nullptr;
+
+		// Вычислить в векторе приоритетов элементов позицию первого приоритета с наименьшим значением
+		auto minItemIterator = std::min_element(priorities.cbegin(), priorities.cend());
+		int minItemIndex = std::distance(priorities.cbegin(), minItemIterator);
+
+		// Запомнить элемент в векторе элементов по найденной позиции
+		T* dequeuedItem = items[minItemIndex];
+
+		// Удалить в обоих векторах элементы по найденной позиции
+		items.erase(items.cbegin() + minItemIndex);
+		priorities.erase(priorities.cbegin() + minItemIndex);
+
+		// Вернуть запомненный элемент
+		return dequeuedItem;
+	}
 };
